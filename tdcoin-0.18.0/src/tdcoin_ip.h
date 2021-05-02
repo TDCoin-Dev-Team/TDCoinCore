673752ed71f942596a724916f1aed92518761432
TATDIG <32492514+tatdig@users.noreply.github.com>
2021-05-02 16:08:56 -0400

    Create tdcoin_ip.h

---
+++ tdcoin-0.18.0/src/tdcoin_ip.h
@@ -0,0 +1,11 @@
+#ifndef TDCOIN_TIPs
+#define TDCOIN_TIPs
+/**
+* TDCoin extension of BIP
+*
+*/
+
+const int TIP001 = 1310; //block height network support funds 500 000 000 (raise total funds to 2 000 000 000 from regular 1 500 000 000)
+const int  TIP002 = 17310; //block height additional funds 100 000 000 (raise total funds to 2 100 000 000)
+
+#endif // TDCOIN_TIPs
